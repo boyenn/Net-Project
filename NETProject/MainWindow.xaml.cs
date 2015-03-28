7605bc42f2236edd7493d767925ae4cf500cdd15
boyenn <boyenvaesen@hotmail.com>
2015-03-28 14:23:49 +0100

    First real commit

---
+++ NETProject/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NETProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StreamReader inputStream = null;
+        private IList<User> userList;
+        private static User currentUser;
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            userList = new List<User>();
+            ReadTextFile();
+            
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (loginTextbox.Text.All(Char.IsLetterOrDigit) && passwordBox.Password.All(Char.IsLetterOrDigit) && !loginTextbox.Text.Equals("") && !passwordBox.Password.Equals(""))
+            {
+
+                foreach (User user in userList)
+                {
+
+                    if (string.Equals(loginTextbox.Text, user.UserName, StringComparison.OrdinalIgnoreCase))
+                    {
+
+                        if (passwordBox.Password.Equals(user.UserPassword))
+                        {
+                            currentUser = user;
+
+                            MainMenuWindow mainMenu = new MainMenuWindow();
+                        
+                            mainMenu.Show();
+                            this.Close();
+                        }
+                        else {
+                            MessageBox.Show("Foutief wachtwoord ingegeven.");
+                        }
+                    }
+                }
+            }
+            else {
+
+                MessageBox.Show("Velden mogen enkel uit cijfers en letters bestaan en mogen niet leeg zijn.");
+
+            }
+        }
+
+        public void ReadTextFile() {
+
+            string line = "";
+            string[] words = new string[6];
+            string fileToSearch = "users.txt";
+        
+            try
+            {
+                inputStream = File.OpenText(fileToSearch);
+                char seperator = ';';
+
+                line = inputStream.ReadLine();
+
+                while (line != null)
+                {
+                    words = line.Split(seperator);
+
+                    userList.Add(new User(Convert.ToInt32(words[0]), words[1], words[2], Convert.ToInt32(words[3]), Convert.ToInt32(words[4]), Convert.ToInt32(words[5])));
+
+                    line = inputStream.ReadLine();
+
+                }
+            }
+            catch (FileNotFoundException ex) {
+                MessageBox.Show("Error: Bestand niet gevonden: " + fileToSearch);
+            }
+
+            if (inputStream != null) {
+                inputStream.Close();
+            }
+        }
+
+        public static User CurrentUser
+        {
+            get { return currentUser; }
+            set { currentUser = value; }
+        }
+
+        private void loginTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ NETProject/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETProject
+{
+    public class User
+    {
+        private int userId, userType, userPoints, userHighscore;
+        private string userName, userPassword;
+ 
+        public User() { 
+
+        }
+
+        public User(int userId, string userName, string userPassword, int userType, int userPoints, int userHighscore)
+        {
+            this.userId = userId;
+            this.userName = userName;
+            this.userPassword = userPassword;
+            this.userType = userType;
+            this.userPoints = userPoints;
+            this.userHighscore = userHighscore;
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+        }
+
+        public string UserPassword
+        {
+            get { return userPassword; }
+        }
+    }
+}
