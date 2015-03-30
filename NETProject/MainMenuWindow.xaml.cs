bc4e6ab1d0ce7e8c7e0c33ba7b8c9abe7eae5b83
Jasper Beuls <addicted-tobass@hotmail.com>
2015-03-30 15:55:49 +0200

    User Interface
    Niet veel veranderd, afbeelding 1-4 toegevoegd als placeholders voor de
    uiteindelijke afbeeldingen en toegevoegd in de listbox.

--- NETProject/MainMenuWindow.xaml.cs
+++ NETProject/MainMenuWindow.xaml.cs
@@ -23,14 +23,17 @@ namespace NETProject
         {
             InitializeComponent();
             MessageBox.Show(MainWindow.CurrentUser.UserName);
-      
-            
-            for (int i = 0; i < 500; i++)
-            {
-                 UserControl1 uc = new UserControl1("Resources/Images/pxlLogo.png", "PXL JONGE");
-                 uc.grid.Background = new SolidColorBrush(Color.FromRgb((byte)(i%255),(byte) (255-(i%255)), (byte)(i%255)));
-                 lst.Items.Add(uc);
-            }
+
+            UserControl1 mainMenuUserControl = new UserControl1("Resources/Images/1.png", "");
+            UserControl1 game1UserControl = new UserControl1("Resources/Images/2.jpg", "Game 1");
+            UserControl1 game2UserControl = new UserControl1("Resources/Images/3.jpg", "Game 2");
+            UserControl1 game3UserControl = new UserControl1("Resources/Images/4.jpg", "Game 3");
+
+            lst.Items.Add(mainMenuUserControl);
+            lst.Items.Add(game1UserControl);
+            lst.Items.Add(game2UserControl);
+            lst.Items.Add(game3UserControl);
+                 
             
         }
     }
